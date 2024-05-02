22f653dd4f6adbedf3ed0c429dd2c370abeb552a
Sachin <spal7582844508@gmail.com>
2024-05-03 00:56:06 +0530

    add user video model

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors({
     Credential:true
 }))
 app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true})) //for url endoding
 app.use(express.static("public")) //save images in own server
 app.use(cookieParser())
 export{app}
