d6730f7b1432c3ad346a5027dd42828b1db521b1
Sachin <spal7582844508@gmail.com>
2024-05-03 14:44:04 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,17 @@ app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true})) //for url endoding
 app.use(express.static("public")) //save images in own server
 app.use(cookieParser())
-export{app}
+
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+// app.use("/user",userRouter)
+app.use("/api/v1/users",userRouter)
+
+
+// http://localhost/api/v1/users/register
+
+ export{app}
